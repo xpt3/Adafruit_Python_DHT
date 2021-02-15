295930449fa83d86d1dee94178bedc31b76ef6d6
xptñ <user.xpt@gmail.com>
2021-02-15 17:54:09 -0300

    Update platform_detect.py
    Add support to Raspberry Pi 4

--- Adafruit_DHT/platform_detect.py
+++ Adafruit_DHT/platform_detect.py
@@ -109,6 +109,9 @@ def pi_version():
     elif match.group(1) == 'BCM2837':
         # Pi 3b+
         return 3
+    elif match.group(1) == 'BCM2711':
+        # Pi 4
+        return 3
     else:
         # Something else, not a pi.
         return None
